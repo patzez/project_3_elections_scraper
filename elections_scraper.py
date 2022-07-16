716fface0328cef7600f202d49347e4775b53261
patzez <pat.zezulka@gmail.com>
2022-07-16 19:30:00 +0200

    setup

--- elections_scraper.py
+++ elections_scraper.py
@@ -1,1 +1,12 @@
-#test setup
+"""
+elections_scraper.py: Třetí projekt do Engeto Online Python Akademie
+
+author: Patrik Zezulka
+email: pat.zezulka@gmail.com
+discord: patzez#8128
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+print("naimportovano")
