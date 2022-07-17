f0c8084c69dc65aa5b42348fe53c4cae83feb89f
patzez <pat.zezulka@gmail.com>
2022-07-17 11:48:22 +0200

    kod, nazev, udaje o volicich mesta

--- elections_scraper.py
+++ elections_scraper.py
@@ -11,18 +11,10 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    url = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=7&xnumnuts=5104"
+    url = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=13&xnumnuts=7204"
     soup = zpracuj_odpoved_serveru(url)
     mesta = najdi_mesta(soup)
-    udaje_mesta = zjisti_udaje_mesta(mesta)
-    for udaj in udaje_mesta:
-        print(udaj)
-    print(len(zjisti_udaje_mesta(mesta)))
-    #print(mesta)
-    # for mesto in najdi_mesta(soup):
-    #     print(mesto.prettify())
-    #     odkaz = mesto.find("a", href=True)["href"]
-    #     print(odkaz)
+    projdi_jednotliva_mesta(mesta)
 
 
 def zpracuj_odpoved_serveru(url):
@@ -40,12 +32,44 @@ def zjisti_udaje_mesta(mesta):
     for mesto in mesta:
         nazev_mesta = mesto.find("td", {"class": "overflow_name"})
         kod_mesta = mesto.find("td", {"class": "cislo"})
-        url_mesta = mesto.find("a", href=True)
-        if nazev_mesta is None:
-            continue
+        if nazev_mesta:
+            udaje_mesta.append([kod_mesta.text, nazev_mesta.text])
         else:
-            udaje_mesta.append([kod_mesta.text, nazev_mesta.text, url_mesta["href"]])
+            continue
     return udaje_mesta
+
+
+def zjisti_url_mesta(mesta):
+    url_mest = []
+    for mesto in mesta:
+        url_mesta = mesto.find("a", href=True)
+        if url_mesta:
+            url_mest.append("https://volby.cz/pls/ps2017nss/" + url_mesta["href"])
+        else:
+            continue
+    return url_mest
+
+
+def projdi_jednotliva_mesta(mesta):
+    udaje_mesta = zjisti_udaje_mesta(mesta)
+    url_mest = zjisti_url_mesta(mesta)
+
+    for i, url in enumerate(url_mest):
+        soup_mesta = zpracuj_odpoved_serveru(url)
+        volici = soup_mesta.find("table", {"class": "table"})
+        volici_v_seznamu = volici.find_all("td", {"class": "cislo"})[3].text.replace("\xa0", "")
+        vydane_obalky = volici.find_all("td", {"class": "cislo"})[4].text.replace("\xa0", "")
+        platne_hlasy = volici.find_all("td", {"class": "cislo"})[7].text.replace("\xa0", "")
+        udaje_mesta[i].extend((volici_v_seznamu, vydane_obalky, platne_hlasy))
+
+    print(udaje_mesta[-3])
+
+
+
+
+
+
+
 
 
 
