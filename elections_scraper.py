f031ad21fed7d3adacaf100b47cb28a50975274b
patzez <pat.zezulka@gmail.com>
2022-07-18 18:16:34 +0200

    Hlavicka csv tabulky + vysledky obci ulozi jako list

--- elections_scraper.py
+++ elections_scraper.py
@@ -12,18 +12,10 @@ from bs4 import BeautifulSoup
 
 def main():
     url = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=13&xnumnuts=7203"
-    soup = zpracuj_odpoved_serveru(url)
-    mesta = najdi_mesta(soup)
-    url_mest = zjisti_url_mesta(mesta)
-    udaje_mesta = zjisti_udaje_mesta(mesta)
-    volici_mesta = projdi_jednotliva_mesta(url_mest)
-    udaje_stran = projdi_udaje_stran(url_mest)
-    nazvy_stran = projdi_nazvy_stran(url_mest[0])
-    print(len(nazvy_stran))
-    # print(udaje_stran)
-    print(udaje_mesta[0], volici_mesta[0], len(udaje_stran[0]))
-    # for i in range(len(udaje_mesta)):
-    #     print(udaje_mesta[i], volici_mesta[i], url_mest[i], udaje_stran[i])
+    vysledky_obce = vytvor_vysledky_obce(url)
+    print(vysledky_obce)
+    print(len(vysledky_obce))
+
 
 
 def zpracuj_odpoved_serveru(url):
@@ -31,12 +23,14 @@ def zpracuj_odpoved_serveru(url):
     return BeautifulSoup(odpoved_url.text, "html.parser")
 
 
-def najdi_mesta(soup):
+def najdi_mesta(url):
+    soup = zpracuj_odpoved_serveru(url)
     mesta = soup.find_all("tr")
     return mesta
 
 
-def zjisti_udaje_mesta(mesta):
+def zjisti_udaje_mesta(url):
+    mesta = najdi_mesta(url)
     udaje_mesta = []
     for mesto in mesta:
         nazev_mesta = mesto.find("td", {"class": "overflow_name"})
@@ -48,7 +42,8 @@ def zjisti_udaje_mesta(mesta):
     return udaje_mesta
 
 
-def zjisti_url_mesta(mesta):
+def zjisti_url_mesta(url):
+    mesta = najdi_mesta(url)
     url_mest = []
     for mesto in mesta:
         url_mesta = mesto.find("a", href=True)
@@ -61,7 +56,6 @@ def zjisti_url_mesta(mesta):
 
 def projdi_jednotliva_mesta(url_mest):
     volici_mesta = []
-
     for url in url_mest:
         soup_mesta = zpracuj_odpoved_serveru(url)
         volici = soup_mesta.find("table", {"class": "table"})
@@ -69,7 +63,6 @@ def projdi_jednotliva_mesta(url_mest):
         vydane_obalky = volici.find_all("td", {"class": "cislo"})[4].text.replace("\xa0", "")
         platne_hlasy = volici.find_all("td", {"class": "cislo"})[7].text.replace("\xa0", "")
         volici_mesta.append([volici_v_seznamu, vydane_obalky, platne_hlasy])
-
     return volici_mesta
 
 
@@ -81,6 +74,7 @@ def projdi_udaje_stran(url_mest):
         celkova_tabulka = []
         radky_tabulky = []
         celkem_hlasu_mesta = []
+
         for hlas in tabulka_hlasy:
             radky = hlas.find_all("tr")
             celkova_tabulka.extend(radky)
@@ -95,16 +89,36 @@ def projdi_udaje_stran(url_mest):
         for radek in radky_tabulky:
             celkem_hlasu_mesta.append(radek[1].text.replace("\xa0", ""))
         hlasy_stran.append(celkem_hlasu_mesta)
+
     return hlasy_stran
 
 
-def projdi_nazvy_stran(url):
+def projdi_nazvy_stran(url_mesta):
     nazvy_stran = []
-    soup_mesta = zpracuj_odpoved_serveru(url)
+    soup_mesta = zpracuj_odpoved_serveru(url_mesta)
     tabulka_nazvy = soup_mesta.find_all("td", {"class": "overflow_name"})
     for nazev in tabulka_nazvy:
         nazvy_stran.append((nazev).text)
     return nazvy_stran
+
+
+def vytvor_hlavicku_tabulky(url_mesta):
+    hlavicka_tabulky = ["Kód obce", "Název obce", "Voliči v seznamu", "Vydané obálky", "Platné Hlasy"]
+    nazvy_stran = projdi_nazvy_stran(url_mesta)
+    hlavicka_tabulky.extend(nazvy_stran)
+    return hlavicka_tabulky
+
+
+def vytvor_vysledky_obce(url):
+    url_mest = zjisti_url_mesta(url)
+    vysledky_obce = zjisti_udaje_mesta(url)
+    volici_mesta = projdi_jednotliva_mesta(url_mest)
+    vysledky_stran = projdi_udaje_stran(url_mest)
+    for i in range(len(vysledky_obce)):
+        vysledky_obce[i].extend(volici_mesta[i])
+    for j in range(len(vysledky_obce)):
+        vysledky_obce[j].extend(vysledky_stran[j])
+    return vysledky_obce
 
 
 if __name__ == "__main__":
